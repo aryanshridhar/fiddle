c1108cb1f768df0a048aa057181c015e718ecdb2
aryanshridhar <aryanshridhar7@gmail.com>
2022-07-20 18:38:52 +0530

    0.102.1

--- tools/webpack/common/webpack.plugins.js
+++ tools/webpack/common/webpack.plugins.js
@@ -8,10 +8,8 @@ module.exports = [
     __importStar: ['tslib', '__importStar'],
   }),
   new webpack.DefinePlugin({
-    STATIC_DIR: webpack.DefinePlugin.runtimeValue(({ module }) => {
-      const rootDir = module.resourceResolveData.descriptionFileRoot;
-      console.log(path.join(rootDir, './static'));
-      return JSON.stringify(path.join(rootDir, './static'));
+    STATIC_DIR: webpack.DefinePlugin.runtimeValue(() => {
+      return JSON.stringify(path.resolve(__dirname, '../../../static/'));
     }, true),
   }),
 ];
@@ -20,5 +18,4 @@ module.exports = [
 
 // * Use process.cwd() to check the file root path
 // * Use path.resolve insteed of path.join and run
-// * Use the current method (should be at the last priority)
 // * Look through the docs better and figure out the best way
