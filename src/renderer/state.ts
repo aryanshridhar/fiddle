09237e96df5177d1bb19db4d94e96ec205bf55d3
aryanshridhar <aryanshridhar7@gmail.com>
2022-07-30 00:38:43 +0530

    tests: to drop this commit

--- src/renderer/state.ts
+++ src/renderer/state.ts
@@ -46,7 +46,7 @@ import {
 import { getUsername } from '../utils/get-username';
 import { ELECTRON_MIRROR } from './mirror-constants';
 import {
-  // Runner,
+  Runner,
   Installer,
   BaseVersions,
   InstallState,
@@ -175,10 +175,10 @@ export class AppState {
     electronInstall: ELECTRON_INSTALL_PATH,
   });
 
-  // public runner: Promise<Runner> = Runner.create({
-  //   installer: this.installer,
-  //   versions: this.baseVersions,
-  // });
+  public runner: Promise<Runner> = Runner.create({
+    installer: this.installer,
+    versions: this.baseVersions,
+  });
 
   constructor(versions: RunnableVersion[]) {
     makeObservable<AppState, 'setPageHash'>(this, {
