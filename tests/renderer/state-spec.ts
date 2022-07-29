91820943412448d382fcdb2744213a580cd3d42c
aryanshridhar <aryanshridhar7@gmail.com>
2022-07-30 00:13:07 +0530

    tests: to drop this commit

--- tests/renderer/state-spec.ts
+++ tests/renderer/state-spec.ts
@@ -45,12 +45,9 @@ describe('AppState', () => {
     (fetchVersions as jest.Mock).mockResolvedValue(mockVersionsArray);
 
     appState = new AppState(mockVersionsArray);
+    await appState.runner;
 
     ipcRendererManager.removeAllListeners();
-  });
-
-  afterAll(async () => {
-    setTimeout(() => true, 2000);
   });
 
   it('exists', () => {
